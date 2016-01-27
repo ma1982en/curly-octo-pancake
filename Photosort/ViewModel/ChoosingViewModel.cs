aa8e5672c698338047e85bf9cff17f376ad19056
Ma1982en <ma1982en@live.com>
2016-01-27 22:04:28 +0100

    Add crosssliding for touch(incomplete)

--- Photosort/ViewModel/ChoosingViewModel.cs
+++ Photosort/ViewModel/ChoosingViewModel.cs
@@ -4,11 +4,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI.Input;
 
 namespace Photosort.ViewModel
 {
     public class ChoosingViewModel : BindableBase
     {
+        GestureRecognizer _gestureRecognizer = new GestureRecognizer();
 
+        public ChoosingViewModel()
+        {
+            _gestureRecognizer.CrossSliding += OnCrossSlidingToAxe;
+        }
+
+        private void OnCrossSlidingToAxe(GestureRecognizer sender, CrossSlidingEventArgs args)
+        {
+            //args.CrossSlidingState == CrossSlidingState.Started;
+        }
     }
 }
